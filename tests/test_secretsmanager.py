dbb04643e1311271ff2eb7eff7fd2e0bdaa14798
Sonny <samson.shi@capitalone.com>
2018-05-17 10:18:56 -0400

    black on test

--- tests/test_secretsmanager.py
+++ tests/test_secretsmanager.py
@@ -12,65 +12,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from __future__ import absolute_import, division, print_function, unicode_literals
-from .common import BaseTest, functional
+from .common import BaseTest
 
 
 class TestSecretsManager(BaseTest):
 
     def test_secrets_manager_tag_resource(self):
-        session = self.replay_flight_data('test_secrets_manager_tag')
-        client = session(region='us-east-1').client('secretsmanager')
-        og_tags = client.describe_secret(
-                SecretId='c7n-test-key').get('Tags')
+        session = self.replay_flight_data("test_secrets_manager_tag")
+        client = session(region="us-east-1").client("secretsmanager")
+        og_tags = client.describe_secret(SecretId="c7n-test-key").get("Tags")
         self.assertFalse(og_tags)
-        p = self.load_policy({
-            'name': 'secrets-manager-resource',
-            'resource': 'secrets-manager',
-            'actions': [
-                {'type': 'tag',
-                'key': 'new-tag',
-                'value': 'new-value'}
-                ]
+        p = self.load_policy(
+            {
+                "name": "secrets-manager-resource",
+                "resource": "secrets-manager",
+                "actions": [{"type": "tag", "key": "new-tag", "value": "new-value"}],
             },
-            session_factory=session)
+            session_factory=session,
+        )
         resources = p.run()
-        new_tags = client.describe_secret(
-                SecretId='c7n-test-key').get('Tags')
+        new_tags = client.describe_secret(SecretId="c7n-test-key").get("Tags")
         self.assertEqual(len(new_tags), 1)
-        self.assertEqual(new_tags[0].get('Key'), 'new-tag')
+        self.assertEqual(new_tags[0].get("Key"), "new-tag")
 
-        p = self.load_policy({
-            'name': 'secrets-manager-resource',
-            'resource': 'secrets-manager',
-            'actions': [
-                {'type': 'remove-tag',
-                'tags': ['new-tag']}
-                ]
+        p = self.load_policy(
+            {
+                "name": "secrets-manager-resource",
+                "resource": "secrets-manager",
+                "actions": [{"type": "remove-tag", "tags": ["new-tag"]}],
             },
-            session_factory=session)
+            session_factory=session,
+        )
         resources = p.run()
-        new_tags = resources[0]['Tags']
-        new_tags = client.describe_secret(
-                SecretId='c7n-test-key').get('Tags')
+        new_tags = resources[0]["Tags"]
+        new_tags = client.describe_secret(SecretId="c7n-test-key").get("Tags")
         self.assertEqual(len(new_tags), 0)
 
     def test_mark_secret_for_op(self):
-        session = self.replay_flight_data('test_secrets_manager_mark_for_op')
-        client = session(region='us-east-1').client('secretsmanager')
-        og_tags = client.describe_secret(
-                SecretId='c7n-test-key').get('Tags')
+        session = self.replay_flight_data("test_secrets_manager_mark_for_op")
+        client = session(region="us-east-1").client("secretsmanager")
+        og_tags = client.describe_secret(SecretId="c7n-test-key").get("Tags")
         self.assertFalse(og_tags)
-        p = self.load_policy({
-            'name': 'secrets-manager-resource',
-            'resource': 'secrets-manager',
-            'actions': [
-                {'type': 'mark-for-op',
-                'op': 'tag',
-                'days': 1}
-                ]
+        p = self.load_policy(
+            {
+                "name": "secrets-manager-resource",
+                "resource": "secrets-manager",
+                "actions": [{"type": "mark-for-op", "op": "tag", "days": 1}],
             },
-            session_factory=session)
+            session_factory=session,
+        )
         resources = p.run()
-        new_tags = client.describe_secret(
-                SecretId='c7n-test-key').get('Tags')
-        self.assertTrue('tag@' in new_tags[0].get('Value'))
+        new_tags = client.describe_secret(SecretId="c7n-test-key").get("Tags")
+        self.assertTrue("tag@" in new_tags[0].get("Value"))
