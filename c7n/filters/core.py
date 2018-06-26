180836e4457a42206611a7ec9cd70b2089f23458
Sonny <samson.shi@capitalone.com>
2018-06-26 15:25:25 -0400

    add a semaphore to check if value is already retrieved, don't attempt multiple retrievals during a single policy execution

--- c7n/filters/core.py
+++ c7n/filters/core.py
@@ -422,7 +422,7 @@ class ValueFilter(Filter):
     def match(self, i):
         if self.v is None and len(self.data) == 1:
             [(self.k, self.v)] = self.data.items()
-        elif self.v is None:
+        elif self.v is None and not hasattr(self, 'content_semaphore'):
             self.k = self.data.get('key')
             self.op = self.data.get('op')
             if 'value_from' in self.data:
@@ -430,6 +430,7 @@ class ValueFilter(Filter):
                 self.v = values.get_values()
             else:
                 self.v = self.data.get('value')
+            self.content_semaphore = True
             self.vtype = self.data.get('value_type')
 
         if i is None:
