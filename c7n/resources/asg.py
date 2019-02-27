84a7bb355db843e0b5f2b56c58d6ef13032c8e39
Sonny <samson.shi@capitalone.com>
2019-02-27 15:50:13 -0500

    default to empty dict for ami

--- c7n/resources/asg.py
+++ c7n/resources/asg.py
@@ -568,7 +568,7 @@ class ImageAgeFilter(AgeFilter):
 
     def get_resource_date(self, asg):
         cfg = self.launch_info.get(asg)
-        ami = self.images.get(cfg.get('ImageId'), '')
+        ami = self.images.get(cfg.get('ImageId'), {})
         return parse(ami.get(
             self.date_attribute, "2000-01-01T01:01:01.000Z"))
 
