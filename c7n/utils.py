eadfd6d51d29996e8238babec509efe04ee7a46b
Sonny <samson.shi@capitalone.com>
2019-05-31 13:08:32 -0400

    set base safe dumper at the top

--- c7n/utils.py
+++ c7n/utils.py
@@ -35,11 +35,13 @@ from c7n import ipaddress, config
 
 # Try to place nice in lambda exec environment
 # where we don't require yaml
+
+BaseSafeDumper = None
+
 try:
     import yaml
 except ImportError:  # pragma: no cover
     yaml = None
-    BaseSafeDumper = None
 else:
     try:
         from yaml import CSafeLoader as SafeLoader, CSafeDumper as BaseSafeDumper
