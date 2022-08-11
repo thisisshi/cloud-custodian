910caf79fac5d1d937a63c540ea9e2ca17e94068
thisisshi <theli@outlook.com>
2022-08-11 12:38:44 -0700

    libffi

--- tools/dev/dockerpkg.py
+++ tools/dev/dockerpkg.py
@@ -33,7 +33,8 @@ FROM {base_build_image} as build-env
 # pre-requisite distro deps, and build env setup
 RUN adduser --disabled-login --gecos "" custodian
 RUN apt-get --yes update
-RUN apt-get --yes install build-essential curl python3-venv python3-dev --no-install-recommends
+RUN apt-get --yes install build-essential curl python3-venv python3-dev libffi-dev \
+    --no-install-recommends
 RUN python3 -m venv /usr/local
 RUN curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py \
     | python3 - -y --version 1.1.14
