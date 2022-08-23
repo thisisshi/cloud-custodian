26f1b9a605abd11fc45fa85c7b515e4bc8afb740
thisisshi <theli@outlook.com>
2022-08-23 15:54:50 -0700

    switch to argparse

--- tools/c7n_kube/c7n_kube/cli.py
+++ tools/c7n_kube/c7n_kube/cli.py
@@ -1,4 +1,4 @@
-import click
+import argparse
 
 from c7n_kube.server import init
 
@@ -7,22 +7,16 @@ PORT = '8800'
 HOST = '0.0.0.0'
 
 
-@click.group()
 def cli():
     """
     Cloud Custodian Admission Controller
     """
 
-
-@cli.command()
-@click.option('--port')
-@click.option('--policy-dir')
-def serve(port, policy_dir):
-    """
-    Serve Cloud Custodian Admission Controller
-    """
-    port = port or PORT
-    init(int(port), policy_dir)
+    parser = argparse.ArgumentParser(description='Cloud Custodian Admission Controller')
+    parser.add_argument('--port', type=int, help='Server port', nargs='?', default=PORT)
+    parser.add_argument('--policy-dir', type=str, required=True, help='policy directory')
+    args = parser.parse_args()
+    init(args.port, args.policy_dir)
 
 
 if __name__ == '__main__':
