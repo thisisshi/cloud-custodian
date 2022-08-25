75bd1b45997f9509cfe9457f952749666a70162b
thisisshi <theli@outlook.com>
2022-08-24 22:07:59 -0700

    fix

--- tools/c7n_kube/c7n_kube/policy.py
+++ tools/c7n_kube/c7n_kube/policy.py
@@ -155,7 +155,7 @@ class ValidatingControllerMode(K8sEventMode):
         for k, v in value.items():
             if not v:
                 continue
-            matched.append(self.handlers[k](self, request, v))
+            matched.append(self.handlers[k](self, request, v.lower()))
         return all(matched)
 
     def run_resource_set(self, event, resources):
