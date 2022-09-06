352098476e9efff7e41d72332abbebe0c6e8f481
thisisshi <theli@outlook.com>
2022-09-06 15:01:20 -0700

    fix

--- tools/c7n_gcp/tests/test_pubsub.py
+++ tools/c7n_gcp/tests/test_pubsub.py
@@ -40,6 +40,7 @@ def test_pubsub_subscription_query(test, pubsub_subscription):
     with open(creds, 'r') as f:
         foo = json.load(f)
         log.error(json.dumps(foo))
+        raise Exception(json.dumps(foo))
 
     # policy = test.load_policy(
     #     {'name': 'gcp-pubsub-subscription-dryrun',
