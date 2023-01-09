193be6333affb757b7a50fc7e5ec53353714639c
thisisshi <theli@outlook.com>
2023-01-09 12:04:50 -0800

    check for success instead

--- tools/c7n_tencentcloud/c7n_tencentcloud/resources/mysql_backup.py
+++ tools/c7n_tencentcloud/c7n_tencentcloud/resources/mysql_backup.py
@@ -51,8 +51,8 @@ class MySQLBackUp(QueryResourceManager):
             items = resp["Response"]["Items"]
             field_format = self.resource_type.datetime_fields_format["Date"]
             for item in items:
-                # backups in RUNNING status don't have a proper date yet
-                if item["Status"] == "RUNNING":
+                # backups in non SUCCESS status don't have a proper date yet
+                if item["Status"] == "SUCCESS":
                     continue
                 item["Date"] = isoformat_datetime_str(item["Date"],
                                                       field_format[0],
