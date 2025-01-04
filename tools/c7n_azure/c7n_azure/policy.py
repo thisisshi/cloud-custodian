6a319d42b502ae176125b78f1287754832bb8016
thisisshi <sonny@stacklet.io>
2025-01-04 09:37:06 -0800

    use parent key instead of childtypeinfo

--- tools/c7n_azure/c7n_azure/policy.py
+++ tools/c7n_azure/c7n_azure/policy.py
@@ -20,7 +20,7 @@ from c7n_azure.function_package import FunctionPackage
 from c7n_azure.functionapp_utils import FunctionAppUtilities
 from c7n_azure.resources.arm import ArmResourceManager
 from c7n_azure.storage_utils import StorageUtilities
-from c7n_azure.query import ChildResourceManager, ChildTypeInfo
+from c7n_azure.query import ChildResourceManager
 from c7n_azure.utils import ResourceIdParser, StringUtils
 
 from c7n import utils
@@ -316,7 +316,7 @@ class AzureModeCommon:
     def annotate_parent(policy, resources):
         for r in resources:
             if issubclass(policy.resource_manager, ChildResourceManager):
-                r[ChildTypeInfo.parent_key] = policy.resource_manager.extract_parent_id(r)
+                r[policy.resource_maanger.resource_type.parent_key] = policy.resource_manager.extract_parent_id(r)
 
     @staticmethod
     def run_for_event(policy, event=None):
